6a475525fec8b63d1361e73422b9d24c52d09dc7
Humbal Shahi <sh.humble@gmail.com>
2023-06-10 19:23:02 -0400

    lesson-3: create the HousingLocation component and render in Housing component

--- angular/angular-tut/src/app/app.module.ts
+++ angular/angular-tut/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    HousingLocationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/angular-tut/src/app/home/home.component.ts
+++ angular/angular-tut/src/app/home/home.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
-      </form>
-    </section>
-  `,
+  //standalone: true,
+  //imports: [CommonModule],
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
