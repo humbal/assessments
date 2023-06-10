3ef068649e3e0fa47d2ad17fb043f937d276d9e3
Humbal Shahi <sh.humble@gmail.com>
2023-06-10 18:59:18 -0400

    Learning Angular App from Angular.io - lession 2

--- angular/angular-tut/src/app/app.component.ts
+++ angular/angular-tut/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  //standalone: true,
+  //imports:[HomeComponent],
 })
 export class AppComponent {
-  title = 'angular-tut';
+  title = 'homes';
 }
