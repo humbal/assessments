0842921834f6aa76a4efb045fc38a46b5c2db617
Humbal Shahi <sh.humble@gmail.com>
2023-06-10 20:03:27 -0400

    Lesson 4 - Creating an interface and implemented in the `home` component.
     Changes to be committed:
    	modified:   src/app/home/home.component.ts
    	new file:   src/app/housinglocation.ts

--- angular/angular-tut/src/app/home/home.component.ts
+++ angular/angular-tut/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
 // import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  housingLocation: HousingLocation = {
+    id: 999,
+    name: 'Test Home',
+    city: 'Edison',
+    state: 'NJ',
+    photo: 'assests/example-house.jpg',
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 
 }
