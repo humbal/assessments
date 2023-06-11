cf2fe206a3159c6203b25dcc2b35ccdcfa795e92
Humbal Shahi <sh.humble@gmail.com>
2023-06-10 20:27:19 -0400

    Lesson 5 - Add an input parameter to the componet HousingLocation
     Changes to be committed:
    	modified:   src/app/housing-location/housing-location.component.ts

--- angular/angular-tut/src/app/housing-location/housing-location.component.ts
+++ angular/angular-tut/src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./housing-location.component.scss']
 })
 export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
 
 }
